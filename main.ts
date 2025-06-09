8f25e83ca6c051ed3e7d4bf4122f243581a215f8
htoopyaelinn56 <hplinn0@gmail.com>
2025-06-09 08:44:05 +0630

    integrated gemini ai api

--- main.ts
+++ main.ts
@@ -1,17 +1,26 @@
 import TelegramBot from "node-telegram-bot-api";
+import { GoogleGenAI } from "@google/genai";
 
 const TELEGRAM_BOT_TOKEN = Deno.env.get("TELEGRAM_BOT_TOKEN")!;
+const GEMINI_API_KEY = Deno.env.get("GEMINI_API_KEY")!;
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
 Deno.serve(async (_req: Request) => {
   console.log("Received request:", _req.method, _req.url);
   if (_req.method === "POST") {
     try {
       const update = await _req.json() as TelegramBot.Update;
+      await bot.sendChatAction(update.message!.chat.id, "typing");
+
+      const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash-001",
+        contents: update.message!.text!,
+      });
 
       await bot.sendMessage(
         update.message!.chat.id,
-        "hello @" + update.message!.from!.username + "!\nHow are you?",
+        response.text!,
       );
       console.info("Webhook", "Success sent message");
       return new Response("OK", { status: 200 });
